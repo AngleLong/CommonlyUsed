b873f697da59e0de67f15296f4d3789cc6b44320
unknown <753355530@qq.com>
2018-08-02 19:26:45 +0800

    '对主项目进行了相应的依赖'

--- app/src/main/java/com/jinlong/used/MainActivity.java
+++ app/src/main/java/com/jinlong/used/MainActivity.java
@@ -2,12 +2,25 @@ package com.jinlong.used;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_name)
+    TextView mTvName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_name)
+    public void onViewClicked() {
     }
 }
